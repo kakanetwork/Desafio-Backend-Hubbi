2e914f1c0316aa9fb25a1a866a3ded92ddc5f844
kakanetwork <kalvimklain@gmail.com>
2025-10-28 00:06:28 -0300

    feat(admin): Adiciona configuração da interface admin

--- apps/estoque/admin.py
+++ apps/estoque/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+# =================================================================================
 
-# Register your models here.
+from django.contrib import admin
+from .models import Pecas
+
+# =================================================================================
+
+@admin.register(Pecas)
+class PecasAdmin(admin.ModelAdmin):
+    """
+    Configuração da interface admin para o modelo dePecas.
+    """
+    list_display = ('nome', 'quantidade', 'preco', 'criado_data', 'atualizado_data')
+    list_filter = ('quantidade', 'criado_data')
+    search_fields = ('nome', 'descricao')
+    ordering = ('nome',)
+
+# =================================================================================
--- apps/usuarios/admin.py
+++ apps/usuarios/admin.py
@@ -1,3 +1,9 @@
+# =================================================================================
+
 from django.contrib import admin
 
-# Register your models here.
+# =================================================================================
+
+# Não é necessário registrar o modelo User explicitamente, pois ele já está registrado por padrão (model do sistema de autenticação do Django).
+
+# =================================================================================
